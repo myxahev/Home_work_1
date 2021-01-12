ca89b2d598415e99b2dc2642bcdaa12e7e13c083
Dmitry <myxahev2@gmail.com>
2021-01-12 23:34:59 +0500

    completed 1.6

---
+++ 1.6.py
@@ -0,0 +1,9 @@
+a = int(input('input km: '))
+b = int(input('input  max km: '))
+day = 1
+
+while a <= b:
+    a = a + a * 0.1
+    day += 1
+    print(f'{day}-й день: {round(a, 2)}')
+print(f'На {day}-й спорсмен достиг результата - не менее {int(b)} км.')
