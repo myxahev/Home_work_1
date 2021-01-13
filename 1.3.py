462a4eb77c503751c8c519510235e13fcb628e33
myxahev <76581169+myxahev@users.noreply.github.com>
2021-01-13 08:52:19 +0500

    Update 1.3.py

--- 1.3.py
+++ 1.3.py
@@ -1,7 +1,5 @@
-n = input('input number: ')
+n, = input('input number: ')
 nn = n + n
 nnn = n + n + n
-n = int(n)
-nn = int(nn)
-nnn = int(nnn)
+n, nn, nnn = int(n), int(nn), int(nnn)
 print(n,'+', nn, '+', nnn, '=', n+nn+nnn)
